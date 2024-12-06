ceb32ce78f5b8bdb7b9ea620d1982c71090a37b9
Jisoo.R <jisuryu0527@gmail.com>
2024-12-06 21:31:31 +0900

    feat(bookmark-service): 즐겨찾기 추가 & 삭제 로직 추가

--- src/main/java/com/sparta/kidscafe/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/sparta/kidscafe/domain/bookmark/repository/BookmarkRepository.java
@@ -1,7 +1,15 @@
 package com.sparta.kidscafe.domain.bookmark.repository;
 
 import com.sparta.kidscafe.domain.bookmark.entity.Bookmark;
+import com.sparta.kidscafe.domain.cafe.entity.Cafe;
+import com.sparta.kidscafe.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+  Optional<Bookmark> findByUserAndCafe(User user, Cafe cafe);
+
 }
