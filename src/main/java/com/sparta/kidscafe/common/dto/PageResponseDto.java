b139a532e5c2bbfed6a7fa4e343064f71d2430e3
guguggagga <gaeul79_@naver.com>
2024-12-08 21:53:49 +0900

    fix: 오타 수정

--- src/main/java/com/sparta/kidscafe/common/dto/PageResponseDto.java
+++ src/main/java/com/sparta/kidscafe/common/dto/PageResponseDto.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 
 /**
- * 페아징 객체를 반환할때 사용하는 responseDto
+ * 페이징 객체를 반환할때 사용하는 responseDto
  */
 @Getter
 @SuperBuilder(builderMethodName = "createResponseBuilder")
