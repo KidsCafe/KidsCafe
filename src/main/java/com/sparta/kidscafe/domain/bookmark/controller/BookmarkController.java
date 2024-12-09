5bb7e286a906803204dbb199c34b8285fe2e8b30
Jisoo.R <jisuryu0527@gmail.com>
2024-12-09 21:22:07 +0900

    feat(bookmark-service): 즐겨찾기 조회(사용자용) api 추가
    * 즐겨찾기 추가&삭제 api: 인증된 사용자 사용 코드 추가

--- src/main/java/com/sparta/kidscafe/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/com/sparta/kidscafe/domain/bookmark/controller/BookmarkController.java
@@ -1,14 +1,20 @@
 package com.sparta.kidscafe.domain.bookmark.controller;
 
+import com.sparta.kidscafe.common.annotation.Auth;
+import com.sparta.kidscafe.common.dto.AuthUser;
+import com.sparta.kidscafe.common.dto.PageResponseDto;
 import com.sparta.kidscafe.common.dto.StatusDto;
+import com.sparta.kidscafe.domain.bookmark.dto.response.BookmarkUserRetreiveResponseDto;
 import com.sparta.kidscafe.domain.bookmark.service.BookmarkService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,11 +24,13 @@ public class BookmarkController {
 
   private final BookmarkService bookmarkService;
 
+  // 즐겨찾기 추가 & 삭제(토글식)
   @PostMapping("/cafes/{cafeId}/bookmarks")
   public ResponseEntity<StatusDto> toggleBookmark(
+      @Auth AuthUser authUser,
       @Valid @PathVariable(value = "cafeId") Long cafeId) {
 
-    boolean isBookmarked = bookmarkService.toggleBookmark(1L, cafeId);
+    boolean isBookmarked = bookmarkService.toggleBookmark(authUser.getId(), cafeId);
 
     if (isBookmarked) {
       return ResponseEntity.status(HttpStatus.CREATED)
@@ -32,4 +40,14 @@ public class BookmarkController {
     }
   }
 
+  // 즐겨찾기 조회(User용)
+  @GetMapping("/users/bookmarks")
+  public ResponseEntity<PageResponseDto<BookmarkUserRetreiveResponseDto>> getBookmarks(
+      @Auth AuthUser authUser,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+    PageResponseDto<BookmarkUserRetreiveResponseDto> response = bookmarkService.getBookmark(
+        authUser.getId(), page, size);
+    return ResponseEntity.ok(response);
+  }
 }
