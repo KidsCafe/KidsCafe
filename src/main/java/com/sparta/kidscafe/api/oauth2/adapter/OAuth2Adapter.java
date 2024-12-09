3397dbf365657bff0c1b01be5d107d74495b1fcd
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-09 19:08:12 +0900

    feat : OAuth2ProviderType 추가 & 설정

--- src/main/java/com/sparta/kidscafe/api/oauth2/adapter/OAuth2Adapter.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/adapter/OAuth2Adapter.java
@@ -1,5 +1,6 @@
 package com.sparta.kidscafe.api.oauth2.adapter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.sparta.kidscafe.api.oauth2.config.OAuth2Properties;
@@ -8,6 +9,11 @@ import com.sparta.kidscafe.api.oauth2.service.OAuth2ProviderType;
 
 public class OAuth2Adapter {
 	public static Map<OAuth2ProviderType, OAuth2ProviderProperties> getOAuth2Provider(OAuth2Properties properties) {
-		return null;
+		Map<OAuth2ProviderType, OAuth2ProviderProperties> providers = new HashMap<>();
+		properties.getUser().forEach((key, value) -> {
+			OAuth2ProviderType providerType = OAuth2ProviderType.of(key);
+			providers.put(providerType, new OAuth2ProviderProperties(value, properties.getProvider().get(key)));
+		});
+		return providers;
 	}
 }
--- src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2ProviderType.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2ProviderType.java
@@ -1,4 +1,22 @@
 package com.sparta.kidscafe.api.oauth2.service;
 
-public class OAuth2ProviderType {
+public enum OAuth2ProviderType {
+	GOOGLE("google"),
+	KAKAO("kakao"),
+	NAVER("naver");
+
+	private final String providerType;
+
+	OAuth2ProviderType(String providerType) {
+		this.providerType = providerType;
+	}
+
+	public static OAuth2ProviderType of(String provideName){
+		for(OAuth2ProviderType providerType : OAuth2ProviderType.values()){
+			if(providerType.providerType.equals(provideName)){
+				return providerType;
+			}
+		}
+		throw new IllegalArgumentException("제공하지 않는 Provider 입니다.");
+	}
 }
