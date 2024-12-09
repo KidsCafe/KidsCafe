55a362d2fd1abaf3bbcc8601f8ba1058ad49791f
gyeonwoo-jerry <gyeonwoo2916@google.com>
2024-12-09 10:44:36 +0900

    -feat : 특정키즈카페리뮤목록 조회 기능 구현, service부분 예외 커스텀 적용, test코드 완료

--- src/main/java/com/sparta/kidscafe/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/kidscafe/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,12 @@
 package com.sparta.kidscafe.domain.review.repository;
 
 import com.sparta.kidscafe.domain.review.entity.Review;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+  Page<Review> findByCafeId(Long cafeId, Pageable pageable);
 }
