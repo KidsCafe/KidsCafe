bc71116ee822c541c34d65fe5eb6c88bf457fe29
KIMHYEJIN <0411khj@naver.com>
2024-12-09 15:07:08 +0900

    test: 가격 정책 관련 테스트 코드 작성 #36
    - PricePolicyControllerTest: 가격 정책 추가 성공/실패 케이스 테스트
    - PricePolicyDummyTest: 가격 정책 Dummy 데이터 생성 테스트
    - PricePolicyServiceTest: 가격 정책 추가 서비스 로직 테스트

--- src/test/java/com/sparta/kidscafe/domain/pricepolicy/dummy/PricePolicyDummyTest.java
+++ src/test/java/com/sparta/kidscafe/domain/pricepolicy/dummy/PricePolicyDummyTest.java
@@ -1,5 +1,26 @@
 package com.sparta.kidscafe.domain.pricepolicy.dummy;
 
-public class PricePolicyDummyTest {
+import com.sparta.kidscafe.common.enums.TargetType;
+import com.sparta.kidscafe.domain.pricepolicy.entity.PricePolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PricePolicyDummyTest {
+
+    @Test
+    @DisplayName("가격 정책 Dummy 데이터 생성 테스트")
+    void createDummyPricePolicy() {
+        PricePolicy pricePolicy = PricePolicy.builder()
+                .targetType(TargetType.PEOPLE)
+                .targetId(1L)
+                .title("주말 추가 요금")
+                .dayType("월, 화, 수, 목, 금, 토, 일")
+                .rate(10.00)
+                .build();
+
+        assertThat(pricePolicy.getTitle()).isEqualTo("주말 추가 요금");
+        assertThat(pricePolicy.getRate()).isEqualTo(10.00);
+    }
 }
