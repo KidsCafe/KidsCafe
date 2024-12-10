bb06c72d3a3ecf862f92157a46c9389de6fbbf18
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-10 18:40:32 +0900

    fix : OAuth2 관련 임시 주석처리 해제

--- src/main/java/com/sparta/kidscafe/api/oauth2/config/InMemoryProviderRepository.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/config/InMemoryProviderRepository.java
@@ -1,20 +1,20 @@
-// package com.sparta.kidscafe.api.oauth2.config;
-//
-// import java.util.HashMap;
-// import java.util.Map;
-//
-// import com.sparta.kidscafe.api.oauth2.provider.OAuth2ProviderProperties;
-// import com.sparta.kidscafe.api.oauth2.service.OAuth2ProviderType;
-//
-// public class InMemoryProviderRepository {
-//
-// 	private final Map<OAuth2ProviderType, OAuth2ProviderProperties> providers;
-//
-// 	public InMemoryProviderRepository(Map<OAuth2ProviderType, OAuth2ProviderProperties> providers) {
-// 		this.providers = new HashMap<>(providers);
-// 	}
-//
-// 	public OAuth2ProviderProperties findProvider(OAuth2ProviderType name){
-// 		return providers.get(name);
-// 	}
-// }
+package com.sparta.kidscafe.api.oauth2.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sparta.kidscafe.api.oauth2.provider.OAuth2ProviderProperties;
+import com.sparta.kidscafe.api.oauth2.service.OAuth2ProviderType;
+
+public class InMemoryProviderRepository {
+
+	private final Map<OAuth2ProviderType, OAuth2ProviderProperties> providers;
+
+	public InMemoryProviderRepository(Map<OAuth2ProviderType, OAuth2ProviderProperties> providers) {
+		this.providers = new HashMap<>(providers);
+	}
+
+	public OAuth2ProviderProperties findProvider(OAuth2ProviderType name){
+		return providers.get(name);
+	}
+}
--- src/main/java/com/sparta/kidscafe/api/oauth2/service/RestTemplateOAuth2ClientService.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/service/RestTemplateOAuth2ClientService.java
@@ -1,9 +1,9 @@
-// package com.sparta.kidscafe.api.oauth2.service;
-//
-// import org.springframework.web.client.RestTemplate;
-//
-// public class RestTemplateOAuth2ClientService implements OAuth2ClientService{
-// 	public RestTemplateOAuth2ClientService(RestTemplate restTemplate) {
-// 		super();
-// 	}
-// }
+package com.sparta.kidscafe.api.oauth2.service;
+
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateOAuth2ClientService implements OAuth2ClientService{
+	public RestTemplateOAuth2ClientService(RestTemplate restTemplate) {
+		super();
+	}
+}
