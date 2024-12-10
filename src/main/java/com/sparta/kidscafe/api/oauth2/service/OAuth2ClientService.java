bd45aba7ad920ecb6463a30576eebb484d6a3e89
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-10 18:40:50 +0900

    fix : OAuth2 관련 임시 주석처리 해제2

--- src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2ClientService.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2ClientService.java
@@ -1,4 +1,10 @@
-// package com.sparta.kidscafe.api.oauth2.service;
-//
-// public interface OAuth2ClientService {
-// }
+package com.sparta.kidscafe.api.oauth2.service;
+
+import com.sparta.kidscafe.api.oauth2.controller.dto.OAuthUserProfile;
+import com.sparta.kidscafe.api.oauth2.provider.OAuth2ProviderProperties;
+
+public interface OAuth2ClientService {
+	String requestAccessToken(String authorizationCode, OAuth2ProviderProperties provider);
+
+	OAuthUserProfile requestUserProfile(String oAuthAccessToken, String providerName, OAuth2ProviderProperties provider);
+}
