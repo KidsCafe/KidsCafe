76e11d1453bee8c51f21f187587db978da5ee21f
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-10 18:41:38 +0900

    feat : OAuth2Service 로직 추가

--- src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2Service.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2Service.java
@@ -1,15 +1,39 @@
-// package com.sparta.kidscafe.api.oauth2.service;
-//
-// import org.springframework.stereotype.Service;
-//
-// import com.sparta.kidscafe.api.auth.controller.dto.SigninResponseDto;
-//
-// import lombok.RequiredArgsConstructor;
-//
-// @Service
-// @RequiredArgsConstructor
-// public class OAuth2Service {
-// 	public SigninResponseDto signinOrRegister(String provider, String code) {
-// 		return null;
-// 	}
-// }
+package com.sparta.kidscafe.api.oauth2.service;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.kidscafe.api.auth.controller.dto.SigninResponseDto;
+import com.sparta.kidscafe.api.auth.service.AuthService;
+import com.sparta.kidscafe.api.oauth2.config.InMemoryProviderRepository;
+import com.sparta.kidscafe.api.oauth2.controller.dto.OAuthUserProfile;
+import com.sparta.kidscafe.api.oauth2.provider.OAuth2ProviderProperties;
+import com.sparta.kidscafe.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2Service {
+
+	private final UserRepository userRepository;
+	private final InMemoryProviderRepository inMemoryProviderRepository;
+	private final OAuth2ClientService oAuth2ClientService;
+	private final AuthService authService;
+
+	public SigninResponseDto signinOrRegister(String providerName, String authorizationCode) throws JsonProcessingException {
+		OAuth2ProviderType providerType = OAuth2ProviderType.of(providerName);
+		OAuth2ProviderProperties provider = inMemoryProviderRepository.findProvider(providerType);
+		String oAuthAccessToken = oAuth2ClientService.requestAccessToken(authorizationCode, provider);
+		OAuthUserProfile oAuthUserProfile = oAuth2ClientService.requestUserProfile(oAuthAccessToken, providerName, provider);
+
+		if(!userRepository.existsByOAuthId(oAuthUserProfile.getOAuthId())){
+			String oAuthId = oAuthUserProfile.getOAuthId();
+			String email = oAuthUserProfile.getEmail();
+			String nickname = oAuthUserProfile.getNickname();
+
+			authService.signupWithOAuth(oAuthId, email, nickname);
+		}
+		return authService.signinWithOAuth(oAuthUserProfile.getEmail());
+	}
+}
