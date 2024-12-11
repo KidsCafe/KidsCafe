fc3d168375fb9da24a6a3e9b005431e2e36fc809
Jisoo.R <jisuryu0527@gmail.com>
2024-12-11 15:22:01 +0900

    feat(reservation-controller): 예약 생성 api 추가 #64

--- src/main/java/com/sparta/kidscafe/domain/reservation/dto/request/ReservationCreateRequestDto.java
+++ src/main/java/com/sparta/kidscafe/domain/reservation/dto/request/ReservationCreateRequestDto.java
@@ -1,4 +1,30 @@
 package com.sparta.kidscafe.domain.reservation.dto.request;
 
+import com.sparta.kidscafe.common.enums.TargetType;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationCreateRequestDto {
+
+  private LocalDateTime startedAt;
+  private LocalDateTime finishedAt;
+  private List<ReservationDetailRequestDto> details;
+
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class ReservationDetailRequestDto {
+
+    private TargetType targetType;
+    private Long targetId;
+    private int count;
+  }
 }
