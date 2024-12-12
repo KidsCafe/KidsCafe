19ee35c19aea02b041ab810bc0e9371786e93d50
KIMHYEJIN <0411khj@naver.com>
2024-12-12 20:18:53 +0900

    feat: UserProfileController 생성 및 USER 프로필 조회 API 구현 #6
    - /api/users/profile 엔드포인트 추가
    - UserProfileController 클래스 생성
    - @Auth 어노테이션을 사용하여 인증된 사용자 정보 처리
    - RoleType.USER 권한 검증 추가 (권한이 없는 사용자는 FORBIDDEN 예외 처리)
    - UserProfileService를 호출하여 사용자 프로필 데이터를 조회
    - ResponseDto를 활용하여 응답 데이터 통일

---
+++ src/main/java/com/sparta/kidscafe/domain/user/controller/UserProfileController.java
@@ -0,0 +1,34 @@
+package com.sparta.kidscafe.domain.user.controller;
+
+import com.sparta.kidscafe.common.annotation.Auth;
+import com.sparta.kidscafe.common.dto.AuthUser;
+import com.sparta.kidscafe.common.dto.ResponseDto;
+import com.sparta.kidscafe.common.enums.RoleType;
+import com.sparta.kidscafe.domain.user.dto.response.UserProfileResponseDto;
+import com.sparta.kidscafe.domain.user.service.UserProfileService;
+import com.sparta.kidscafe.exception.BusinessException;
+import com.sparta.kidscafe.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    @GetMapping("/api/users/profile")
+    public ResponseEntity<ResponseDto<UserProfileResponseDto>> getUserProfile(@Auth AuthUser authUser) {
+        // 인증된 사용자 권한 확인
+        if (authUser.getRoleType() != RoleType.USER) {
+            throw new BusinessException(ErrorCode.FORBIDDEN);
+        }
+
+        // 서비스 계층에서 프로필 조회
+        ResponseDto<UserProfileResponseDto> responseDto = userProfileService.getUserProfile(authUser);
+
+        return ResponseEntity.ok(responseDto);
+    }
+}
