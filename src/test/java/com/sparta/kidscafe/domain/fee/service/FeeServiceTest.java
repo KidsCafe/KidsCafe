f41dd8e77f79bc73d10c0fe895208face48d6724
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-12 15:03:13 +0900

    test: FeeServiceTest -> 권한별로 가격표 생성 단위 테스트(부정 테스트)

--- src/test/java/com/sparta/kidscafe/domain/fee/service/FeeServiceTest.java
+++ src/test/java/com/sparta/kidscafe/domain/fee/service/FeeServiceTest.java
@@ -1,5 +1,6 @@
 package com.sparta.kidscafe.domain.fee.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Optional;
@@ -10,8 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.sparta.kidscafe.common.dto.AuthUser;
 import com.sparta.kidscafe.common.enums.AgeGroup;
@@ -21,7 +20,9 @@ import com.sparta.kidscafe.domain.cafe.repository.CafeRepository;
 import com.sparta.kidscafe.domain.fee.dto.request.FeeCreateRequestDto;
 import com.sparta.kidscafe.domain.fee.entity.Fee;
 import com.sparta.kidscafe.domain.fee.repository.FeeRepository;
-
+import com.sparta.kidscafe.domain.user.entity.User;
+import com.sparta.kidscafe.exception.BusinessException;
+import com.sparta.kidscafe.exception.ErrorCode;
 
 @ExtendWith(MockitoExtension.class)
 public class FeeServiceTest {
@@ -38,7 +39,7 @@ public class FeeServiceTest {
 	private FeeService feeService;
 
 	@Test
-	@DisplayName("가격표 생성 : OWNER 권한 - 성공")
+	@DisplayName("가격표 생성: OWNER 권한-본인가게-성공")
 	void createFee_owner_success(){
 		// given
 		AuthUser authUser = new AuthUser(1L, "owner@test.com", RoleType.OWNER);
@@ -47,7 +48,7 @@ public class FeeServiceTest {
 			.user(authUser.toUser())
 			.build();
 
-		FeeCreateRequestDto feeCreateRequestDto = new FeeCreateRequestDto(AgeGroup.CHILDREN, 1000);
+		FeeCreateRequestDto feeCreateRequestDto = new FeeCreateRequestDto(AgeGroup.CHILDREN, 10000);
 
 		when(cafeRepository.findById(1L)).thenReturn(Optional.of(cafe));
 		when(feeRepository.save(any(Fee.class))).thenReturn(mock(Fee.class));
@@ -58,6 +59,46 @@ public class FeeServiceTest {
 		// then
 		verify(cafeRepository, times(1)).findById(1L);
 		verify(feeRepository, times(1)).save(any(Fee.class));
-
 	}
+
+	@Test
+	@DisplayName("가격표 생성: OWNER 권한-본인 가게 X-실패")
+	void createFee_owner_fail(){
+		// given
+		AuthUser authUser = new AuthUser(1L, "owner@test.com", RoleType.OWNER);
+		Cafe otherCafe = Cafe.builder()
+			.id(2L)
+			.user(mock(User.class))
+			.build();
+
+		FeeCreateRequestDto feeCreateRequestDto = new FeeCreateRequestDto(AgeGroup.TEENAGER, 10000);
+
+		when(cafeRepository.findById(2L)).thenReturn(Optional.of(otherCafe));
+
+		// when // then
+		assertThatThrownBy(() -> feeService.createFee(authUser, 2L, feeCreateRequestDto))
+			.isInstanceOf(BusinessException.class)
+			.hasMessageContaining(String.valueOf(ErrorCode.FEE_TABLE_OWN_CREATE));
+
+		verify(cafeRepository, times(1)).findById(2L);
+		verify(feeRepository, never()).save(any());
+	}
+
+	@Test
+	@DisplayName("가격표 생성: USER 권한 - 실패")
+	void createFee_user_fail(){
+		// given
+		AuthUser authUser = new AuthUser(1L, "user@test.com", RoleType.USER);
+		FeeCreateRequestDto feeCreateRequestDto = new FeeCreateRequestDto(AgeGroup.CHILDREN, 10000);
+
+		// when // then
+		assertThatThrownBy(() -> feeService.createFee(authUser, 1L, feeCreateRequestDto))
+			.isInstanceOf(BusinessException.class)
+			.hasMessageContaining(String.valueOf(ErrorCode.FEE_TABLE_UNAUTHORIZED));
+
+		verify(cafeRepository, never()).findById(any());
+		verify(feeRepository, never()).save(any());
+	}
+
+
 }
