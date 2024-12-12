958bee5d776eb507a3f1e8495609cf46c5636221
Jisoo.R <jisuryu0527@gmail.com>
2024-12-13 00:59:09 +0900

    feat(reservation-entity): 예약 승인 기능 추가 #117
    * ReservationStatus.java 추가

---
+++ src/main/java/com/sparta/kidscafe/domain/reservation/enums/ReservationStatus.java
@@ -0,0 +1,10 @@
+package com.sparta.kidscafe.domain.reservation.enums;
+
+public enum ReservationStatus {
+  PENDING,
+  APPROVED,
+  CONFIRMED,
+  CANCELLED_BY_USER,
+  CANCELLED_BY_OWNER,
+  COMPLETED
+}
--- src/main/java/com/sparta/kidscafe/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/sparta/kidscafe/domain/reservation/repository/ReservationRepository.java
@@ -1,13 +1,16 @@
 package com.sparta.kidscafe.domain.reservation.repository;
 
 import com.sparta.kidscafe.domain.reservation.entity.Reservation;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
   Page<Reservation> findByUserId(Long userId, Pageable pageable);
 
   Page<Reservation> findByCafeId(Long cafeId, Pageable pageable);
+
 }
