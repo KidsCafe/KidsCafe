ca7e8af7e6c1fe6adcaccace6ec062f6c0cadb45
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-12 12:57:00 +0900

    refactor: FeeController, FeeService -> 코드 컨벤션에 맞게 수정

--- src/main/java/com/sparta/kidscafe/domain/fee/service/FeeService.java
+++ src/main/java/com/sparta/kidscafe/domain/fee/service/FeeService.java
@@ -3,9 +3,12 @@ package com.sparta.kidscafe.domain.fee.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.sparta.kidscafe.common.dto.AuthUser;
+import com.sparta.kidscafe.common.dto.ListResponseDto;
+import com.sparta.kidscafe.common.dto.StatusDto;
 import com.sparta.kidscafe.common.enums.RoleType;
 import com.sparta.kidscafe.domain.cafe.entity.Cafe;
 import com.sparta.kidscafe.domain.cafe.repository.CafeRepository;
@@ -44,35 +47,42 @@ public class FeeService {
 	}
 
 	@Transactional
-	public FeeResponseDto createFee(AuthUser authUser, Long cafeId, FeeCreateRequestDto feeCreateRequestDto) {
+	public StatusDto createFee(AuthUser authUser, Long cafeId, FeeCreateRequestDto feeCreateRequestDto) {
 		Cafe cafe = cafeRepository.findById(cafeId)
 			.orElseThrow(() -> new BusinessException(ErrorCode.CAFE_NOT_FOUND));
 
 		validationOwner(authUser, cafe);
-
 		Fee fee = feeCreateRequestDto.convertDtoToEntity(cafe);
-		Fee savedFee = feeRepository.save(fee);
-		return FeeResponseDto.from(savedFee);
+
+		feeRepository.save(fee);
+
+		return StatusDto.builder()
+			.status(HttpStatus.OK.value())
+			.message(fee.getAgeGroup().getName() + "가격표 생성")
+			.build();
 	}
 
-	public List<FeeResponseDto> getFeesByCafe(Long cafeId) {
+	public ListResponseDto<FeeResponseDto> getFeesByCafe(Long cafeId) {
 		List<Fee> fees = feeRepository.findByCafeId(cafeId);
-		return fees.stream()
-			.map(FeeResponseDto::from)
-			.collect(Collectors.toList());
+		return ListResponseDto.success(
+			fees.stream()
+				.map(FeeResponseDto::from)
+				.collect(Collectors.toList()), HttpStatus.OK, "가격표 조회");
 	}
 
 	@Transactional
-	public FeeResponseDto updateFee(AuthUser authUser, Long cafeId, Long feeId, FeeUpdateRequestDto feeUpdateRequestDto) {
+	public StatusDto updateFee(AuthUser authUser, Long cafeId, Long feeId, FeeUpdateRequestDto feeUpdateRequestDto) {
 		Cafe cafe = cafeRepository.findById(cafeId)
 			.orElseThrow(() -> new BusinessException(ErrorCode.CAFE_NOT_FOUND));
-
 		Fee fee = feeRepository.findById(feeId)
 			.orElseThrow(() -> new BusinessException(ErrorCode.FEE_NOT_FOUND));
 
 		validationOwner(authUser, cafe);
-
 		fee.update(feeUpdateRequestDto);
-		return FeeResponseDto.from(fee);
+
+		return StatusDto.builder()
+			.status(HttpStatus.OK.value())
+			.message(fee.getFee() + "원으로 " + "id=" + fee.getId() + "의 " + fee.getAgeGroup().getName() + " 가격 수정")
+			.build();
 	}
 }
