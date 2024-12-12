7b41db2a89b4b95137ba46a7d00a986efc708f48
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-12 19:22:21 +0900

    refactor: OAuth2Attributes -> OAuth2 서버에서 받아온 유저 정보 추출 및 매핑

--- src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2Attributes.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/service/OAuth2Attributes.java
@@ -6,53 +6,31 @@ import java.util.Map;
 import com.sparta.kidscafe.api.oauth2.controller.dto.OAuth2UserProfile;
 
 public enum OAuth2Attributes {
-	KAKAO("kakao"){
+	// 일단 깃헙만 추가 -> 테스트 완료되면 다른 소셜도 추가 예정
+	GITHUB("github"){
 		@Override
 		public OAuth2UserProfile of(Map<String, Object> attributes){
-			Map<String, Object> containEmailResponse = ((Map<String, Object>)attributes.get("kakao_account"));
-			Map<String, Object> containNameResponse = ((Map<String, Object>)attributes.get("properties"));
 			return OAuth2UserProfile.builder()
-				.oauthId(attributes.get("id").toString())
-				.email((String)containEmailResponse.get("email"))
-				.name((String)containNameResponse.get("nickname"))
-				.build();
-		}
-	},
-	NAVER("naver") {
-		@Override
-		public OAuth2UserProfile of(Map<String, Object> attributes) {
-			Map<String, Object> response = (Map<String, Object>)attributes.get("response");
-			return OAuth2UserProfile.builder()
-				.oauthId((String)response.get("id"))
-				.email((String)response.get("email"))
-				.name((String)response.get("name"))
-				.build();
-		}
-	},
-	GOOGLE("google") {
-		@Override
-		public OAuth2UserProfile of(Map<String, Object> attributes) {
-			return OAuth2UserProfile.builder()
-				.oauthId((String)attributes.get("id"))
-				.email((String)attributes.get("email"))
-				.name((String)attributes.get("name"))
+				.oauthId(String.valueOf(attributes.get("id")))
+				.email((String) attributes.get("email"))
+				.name((String) attributes.get("name"))
 				.build();
 		}
 	};
 
 	private final String providerName;
 
-	public abstract OAuth2UserProfile of(Map<String, Object> attributes);
-
 	OAuth2Attributes(String name){
 		this.providerName = name;
 	}
 
-	public static OAuth2UserProfile extract(String providerName, Map<String, Object> attributes) {
+	public static OAuth2UserProfile extract(String providerName, Map<String, Object> attributes){
 		return Arrays.stream(values())
 			.filter(provider -> providerName.equals(provider.providerName))
 			.findFirst()
-			.orElseThrow(IllegalAccessError::new)
+			.orElseThrow(IllegalArgumentException::new)
 			.of(attributes);
 	}
+
+	public abstract OAuth2UserProfile of(Map<String, Object> attributes);
 }
