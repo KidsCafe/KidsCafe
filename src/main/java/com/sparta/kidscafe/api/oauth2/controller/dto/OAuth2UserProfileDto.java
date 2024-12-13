568c46cbc121f281d218c5d8a05373b883ec2cda
Dohyeon_Parrk <parkyj3213@naver.com>
2024-12-13 21:43:43 +0900

    refactor: OAuth2 서버로부터 받은 사용자 정보 받는 DTO 수정

--- src/main/java/com/sparta/kidscafe/api/oauth2/controller/dto/OAuth2UserProfileDto.java
+++ src/main/java/com/sparta/kidscafe/api/oauth2/controller/dto/OAuth2UserProfileDto.java
@@ -1,5 +1,6 @@
 package com.sparta.kidscafe.api.oauth2.controller.dto;
 
+import com.sparta.kidscafe.common.enums.LoginType;
 import com.sparta.kidscafe.common.enums.RoleType;
 import com.sparta.kidscafe.domain.user.entity.User;
 
@@ -12,20 +13,28 @@ public class OAuth2UserProfileDto {
 	private final String oauthId;
 	private final String email;
 	private final String name;
+	private final String address;
+	private final String nickname;
 
 	@Builder
-	public OAuth2UserProfileDto(String oauthId, String email, String name) {
+	public OAuth2UserProfileDto(String oauthId, String email, String name, String address, String nickname) {
 		this.oauthId = oauthId;
 		this.email = email;
 		this.name = name;
+		this.address = address;
+		this.nickname = nickname;
 	}
 
 	public User toUser(){
 		return User.builder()
 			.oauthId(oauthId)
-			.email(email)
+			.email(email != null ? email : "")
+			.password("UNKNOWN")
+			.nickname(nickname != null ? nickname : "")
 			.name(name)
+			.address(address != null ? address : "")
 			.role(RoleType.USER)
+			.loginType(LoginType.OAUTH)
 			.build();
 	}
 }
