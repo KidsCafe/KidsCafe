e4f64b50a7b59096ef07c556189aa14223bea59e
Jisoo.R <jisuryu0527@gmail.com>
2024-12-16 16:26:08 +0900

    feat(reservation-entity): 예약 수정 api 추가 #114

---
+++ src/main/java/com/sparta/kidscafe/domain/reservation/dto/request/ReservationUpdateRequestDto.java
@@ -0,0 +1,37 @@
+package com.sparta.kidscafe.domain.reservation.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationUpdateRequestDto {
+
+  @NotNull
+  private String startedAt;
+
+  @NotNull
+  private String finishedAt;
+
+  @Valid
+  private List<ReservationDetailUpdateRequestDto> details;
+
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class ReservationDetailUpdateRequestDto {
+
+    @NotNull
+    private Long targetId;
+    @NotNull
+    private Long count;
+  }
+}
+
