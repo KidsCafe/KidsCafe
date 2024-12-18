abbae97b7ca3b94a3b27388bd3cf282fa09d2f9d
KIMHYEJIN <0411khj@naver.com>
2024-12-18 19:36:08 +0900

    feat: 네이버 카페 검색 기능 서비스 계층에 추가 - CafeService에 NaverMapService 의존성 추가 - 네이버 API 호출을 통해 키즈카페 데이터를 검색하는 기능 구현 - searchCafesFromNaver 메서드 추가 및 예외 처리 추가

--- src/main/java/com/sparta/kidscafe/domain/cafe/service/CafeService.java
+++ src/main/java/com/sparta/kidscafe/domain/cafe/service/CafeService.java
@@ -11,6 +11,7 @@ import com.sparta.kidscafe.domain.cafe.dto.request.CafeSimpleRequestDto;
 import com.sparta.kidscafe.domain.cafe.dto.request.CafesSimpleCreateRequestDto;
 import com.sparta.kidscafe.domain.cafe.dto.response.CafeDetailResponseDto;
 import com.sparta.kidscafe.domain.cafe.dto.response.CafeResponseDto;
+import com.sparta.kidscafe.domain.cafe.dto.response.NaverCafeResponseDto;
 import com.sparta.kidscafe.domain.cafe.entity.Cafe;
 import com.sparta.kidscafe.domain.cafe.entity.CafeImage;
 import com.sparta.kidscafe.domain.cafe.repository.CafeImageRepository;
@@ -23,143 +24,152 @@ import com.sparta.kidscafe.domain.pricepolicy.repository.PricePolicyRepository;
 import com.sparta.kidscafe.domain.room.entity.Room;
 import com.sparta.kidscafe.domain.room.repository.RoomRepository;
 import com.sparta.kidscafe.domain.user.entity.User;
-import com.sparta.kidscafe.domain.user.repository.UserRepository;
 import com.sparta.kidscafe.exception.BusinessException;
 import com.sparta.kidscafe.exception.ErrorCode;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CafeService {
 
-  private final CafeRepository cafeRepository;
-  private final CafeImageRepository cafeImageRepository;
-  private final RoomRepository roomRepository;
-  private final FeeRepository feeRepository;
-  private final PricePolicyRepository pricePolicyRepository;
-  private final UserValidationCheck userValidationCheck;
-  private final CafeValidationCheck cafeValidationCheck;
+    private final CafeRepository cafeRepository;
+    private final CafeImageRepository cafeImageRepository;
+    private final RoomRepository roomRepository;
+    private final FeeRepository feeRepository;
+    private final PricePolicyRepository pricePolicyRepository;
+    private final UserValidationCheck userValidationCheck;
+    private final CafeValidationCheck cafeValidationCheck;
+    private final NaverMapService naverMapService;
 
-  @Transactional
-  public StatusDto createCafe(AuthUser authUser, CafeCreateRequestDto requestDto) {
-    User user = userValidationCheck.validMy(authUser.getId());
-    Cafe cafe = saveCafe(requestDto, user);
-    saveCafeImage(cafe, requestDto.getImages());
-    saveCafeDetailInfo(requestDto, cafe);
-    return createStatusDto(
-        HttpStatus.CREATED,
-        "[" + cafe.getName() + "] 등록 성공"
-    );
-  }
-
-  public StatusDto creatCafe(AuthUser authUser, CafesSimpleCreateRequestDto requestDto) {
-    User user = userValidationCheck.validMy(authUser.getId());
-    List<Cafe> cafes = requestDto.convertDtoToEntity(user);
-    cafeRepository.saveAll(cafes);
-    return createStatusDto(
-        HttpStatus.CREATED,
-        "카페 [" + cafes.size() + "]개 등록 성공"
-    );
-  }
-
-  public PageResponseDto<CafeResponseDto> searchCafe(CafeSearchCondition condition) {
-    Page<CafeResponseDto> cafes = cafeRepository.findAllByCafe(condition);
-    return PageResponseDto.success(
-        cafes,
-        HttpStatus.OK,
-        cafes.isEmpty() ? "조회 결과가 없습니다." : "카페 조회 성공"
-    );
-  }
-
-  public ResponseDto<CafeDetailResponseDto> findCafe(Long cafeId) {
-    CafeResponseDto cafeResponseDto = cafeRepository.findCafeById(cafeId);
-    return ResponseDto.success(
-        createCafeDetailInfo(cafeResponseDto),
-        HttpStatus.OK,
-        cafeResponseDto == null ?
-            "조회 결과가 없습니다."
-            : "[" + cafeResponseDto.getName() + "] 상세 조회 성공"
-    );
-  }
-
-  @Transactional
-  public StatusDto updateCafe(AuthUser authUser, Long cafeId, CafeSimpleRequestDto requestDto) {
-    Cafe cafe = cafeValidationCheck.validMyCafe(cafeId, authUser.getId());
-    cafe.update(requestDto);
-    return createStatusDto(
-        HttpStatus.OK,
-        "[" + cafe.getName() + "] 수정 성공"
-    );
-  }
-
-  @Transactional
-  public void deleteCafe(AuthUser authUser, Long cafeId) {
-    Cafe cafe = cafeValidationCheck.validMyCafe(cafeId, authUser.getId());
-    cafeRepository.delete(cafe);
-    List<CafeImage> cafeImages = cafeImageRepository.findAllByCafeId(cafeId);
-    for (CafeImage cafeImage : cafeImages) {
-      cafeImage.delete();
-    }
-  }
-
-  @Transactional
-  public void deleteCafe(AuthUser authUser, List<Long> cafeIds) {
-    List<Cafe> cafes = cafeRepository.findAllByUserIdAndIdIn(authUser.getId(), cafeIds);
-    cafeRepository.deleteAll(cafes);
-  }
-
-  public void saveCafeImage(Cafe cafe, List<Long> images) {
-    List<CafeImage> cafeImages = cafeImageRepository.findAllById(images);
-    for (CafeImage cafeImage : cafeImages) {
-      cafeImage.update(cafe.getId());
-    }
-  }
-
-  private Cafe saveCafe(CafeCreateRequestDto requestDto, User user) {
-    Cafe cafe = requestDto.convertDtoToEntityByCafe(user);
-    cafeRepository.save(cafe);
-    return cafe;
-  }
-
-  private void saveCafeDetailInfo(CafeCreateRequestDto requestDto, Cafe cafe) {
-    List<Room> rooms = requestDto.convertDtoToEntityByRoom(cafe);
-    List<Fee> fees = requestDto.convertDtoToEntityByFee(cafe);
-    List<PricePolicy> pricePolicies = requestDto.convertDtoToEntityByPricePolicy(cafe);
-
-    roomRepository.saveAll(rooms);
-    feeRepository.saveAll(fees);
-    pricePolicyRepository.saveAll(pricePolicies);
-  }
-
-  private CafeDetailResponseDto createCafeDetailInfo(CafeResponseDto cafeResponseDto) {
-    if (cafeResponseDto == null) {
-      return null;
+    @Transactional
+    public StatusDto createCafe(AuthUser authUser, CafeCreateRequestDto requestDto) {
+        User user = userValidationCheck.validMy(authUser.getId());
+        Cafe cafe = saveCafe(requestDto, user);
+        saveCafeImage(cafe, requestDto.getImages());
+        saveCafeDetailInfo(requestDto, cafe);
+        return createStatusDto(
+                HttpStatus.CREATED,
+                "[" + cafe.getName() + "] 등록 성공"
+        );
     }
 
-    Long cafeId = cafeResponseDto.getId();
-    List<CafeImage> images = cafeImageRepository.findAllByCafeId(cafeId);
-    List<Room> rooms = roomRepository.findAllByCafeId(cafeId);
-    List<Fee> fees = feeRepository.findAllByCafeId(cafeId);
-    List<PricePolicy> pricePolicies = pricePolicyRepository.findAllByCafeId(cafeId);
+    public StatusDto creatCafe(AuthUser authUser, CafesSimpleCreateRequestDto requestDto) {
+        User user = userValidationCheck.validMy(authUser.getId());
+        List<Cafe> cafes = requestDto.convertDtoToEntity(user);
+        cafeRepository.saveAll(cafes);
+        return createStatusDto(
+                HttpStatus.CREATED,
+                "카페 [" + cafes.size() + "]개 등록 성공"
+        );
+    }
 
-    CafeDetailResponseDto cafeDetailResponseDto = new CafeDetailResponseDto();
-    cafeDetailResponseDto.setCafeInfo(cafeResponseDto);
-    cafeDetailResponseDto.setCafeImage(images);
-    cafeDetailResponseDto.setRooms(rooms);
-    cafeDetailResponseDto.setFees(fees);
-    cafeDetailResponseDto.setPricePolicies(pricePolicies);
-    return cafeDetailResponseDto;
-  }
+    public PageResponseDto<CafeResponseDto> searchCafe(CafeSearchCondition condition) {
+        Page<CafeResponseDto> cafes = cafeRepository.findAllByCafe(condition);
+        return PageResponseDto.success(
+                cafes,
+                HttpStatus.OK,
+                cafes.isEmpty() ? "조회 결과가 없습니다." : "카페 조회 성공"
+        );
+    }
 
-  private StatusDto createStatusDto(HttpStatus status, String message) {
-    return StatusDto.builder()
-        .status(status.value())
-        .message(message)
-        .build();
-  }
+    public ResponseDto<CafeDetailResponseDto> findCafe(Long cafeId) {
+        CafeResponseDto cafeResponseDto = cafeRepository.findCafeById(cafeId);
+        return ResponseDto.success(
+                createCafeDetailInfo(cafeResponseDto),
+                HttpStatus.OK,
+                cafeResponseDto == null ?
+                        "조회 결과가 없습니다."
+                        : "[" + cafeResponseDto.getName() + "] 상세 조회 성공"
+        );
+    }
+
+    @Transactional
+    public StatusDto updateCafe(AuthUser authUser, Long cafeId, CafeSimpleRequestDto requestDto) {
+        Cafe cafe = cafeValidationCheck.validMyCafe(cafeId, authUser.getId());
+        cafe.update(requestDto);
+        return createStatusDto(
+                HttpStatus.OK,
+                "[" + cafe.getName() + "] 수정 성공"
+        );
+    }
+
+    @Transactional
+    public void deleteCafe(AuthUser authUser, Long cafeId) {
+        Cafe cafe = cafeValidationCheck.validMyCafe(cafeId, authUser.getId());
+        cafeRepository.delete(cafe);
+        List<CafeImage> cafeImages = cafeImageRepository.findAllByCafeId(cafeId);
+        for (CafeImage cafeImage : cafeImages) {
+            cafeImage.delete();
+        }
+    }
+
+    @Transactional
+    public void deleteCafe(AuthUser authUser, List<Long> cafeIds) {
+        List<Cafe> cafes = cafeRepository.findAllByUserIdAndIdIn(authUser.getId(), cafeIds);
+        cafeRepository.deleteAll(cafes);
+    }
+
+    public void saveCafeImage(Cafe cafe, List<Long> images) {
+        List<CafeImage> cafeImages = cafeImageRepository.findAllById(images);
+        for (CafeImage cafeImage : cafeImages) {
+            cafeImage.update(cafe.getId());
+        }
+    }
+
+    private Cafe saveCafe(CafeCreateRequestDto requestDto, User user) {
+        Cafe cafe = requestDto.convertDtoToEntityByCafe(user);
+        cafeRepository.save(cafe);
+        return cafe;
+    }
+
+    private void saveCafeDetailInfo(CafeCreateRequestDto requestDto, Cafe cafe) {
+        List<Room> rooms = requestDto.convertDtoToEntityByRoom(cafe);
+        List<Fee> fees = requestDto.convertDtoToEntityByFee(cafe);
+        List<PricePolicy> pricePolicies = requestDto.convertDtoToEntityByPricePolicy(cafe);
+
+        roomRepository.saveAll(rooms);
+        feeRepository.saveAll(fees);
+        pricePolicyRepository.saveAll(pricePolicies);
+    }
+
+    private CafeDetailResponseDto createCafeDetailInfo(CafeResponseDto cafeResponseDto) {
+        if (cafeResponseDto == null) {
+            return null;
+        }
+
+        Long cafeId = cafeResponseDto.getId();
+        List<CafeImage> images = cafeImageRepository.findAllByCafeId(cafeId);
+        List<Room> rooms = roomRepository.findAllByCafeId(cafeId);
+        List<Fee> fees = feeRepository.findAllByCafeId(cafeId);
+        List<PricePolicy> pricePolicies = pricePolicyRepository.findAllByCafeId(cafeId);
+
+        CafeDetailResponseDto cafeDetailResponseDto = new CafeDetailResponseDto();
+        cafeDetailResponseDto.setCafeInfo(cafeResponseDto);
+        cafeDetailResponseDto.setCafeImage(images);
+        cafeDetailResponseDto.setRooms(rooms);
+        cafeDetailResponseDto.setFees(fees);
+        cafeDetailResponseDto.setPricePolicies(pricePolicies);
+        return cafeDetailResponseDto;
+    }
+
+    public List<NaverCafeResponseDto> searchCafesFromNaver(String query, String region) {
+        try {
+            return naverMapService.searchCafes(query, region);
+        } catch (Exception e) {
+            throw new BusinessException(ErrorCode.EXTERNAL_API_ERROR, "네이버 API 호출 실패");
+        }
+    }
+
+    private StatusDto createStatusDto(HttpStatus status, String message) {
+        return StatusDto.builder()
+                .status(status.value())
+                .message(message)
+                .build();
+    }
 }
