a027ae55627ae8c87ae18965607bbffb5f9beb5c
gyeonwoo-jerry <gyeonwoo2916@google.com>
2024-12-18 21:37:27 +0900

    -feat : 이미제 제한 완료

--- src/main/java/com/sparta/kidscafe/domain/review/dto/request/ReviewCreateRequestDto.java
+++ src/main/java/com/sparta/kidscafe/domain/review/dto/request/ReviewCreateRequestDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record ReviewCreateRequestDto(
@@ -14,6 +15,7 @@ public record ReviewCreateRequestDto(
     @NotNull
     String content,
     Long parentId,
+    @Size(max = 5, message = "이미지는 최대 5개까지만 첨부할 수 있습니다.")
     List<Long>imageId
 ) {
 }
