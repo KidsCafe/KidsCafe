b9505a7493cbbfc3711603a7f60f587f2c8f33b3
KIMHYEJIN <0411khj@naver.com>
2024-12-18 19:35:13 +0900

    feat: 네이버 API를 통한 키즈카페 검색 기능 추가 - NaverMapService를 사용하여 외부 API 호출 구현 - CafeController에 네이버 카페 검색 엔드포인트 추가 - NaverCafeResponseDto 추가하여 API 응답 데이터 처리 - ErrorCode에 외부 API 오류 처리 코드(EXTERNAL_API_ERROR) 추가

--- src/main/java/com/sparta/kidscafe/domain/cafe/controller/CafeController.java
+++ src/main/java/com/sparta/kidscafe/domain/cafe/controller/CafeController.java
@@ -12,119 +12,128 @@ import com.sparta.kidscafe.domain.cafe.dto.request.CafeSimpleRequestDto;
 import com.sparta.kidscafe.domain.cafe.dto.request.CafesSimpleCreateRequestDto;
 import com.sparta.kidscafe.domain.cafe.dto.response.CafeDetailResponseDto;
 import com.sparta.kidscafe.domain.cafe.dto.response.CafeResponseDto;
+import com.sparta.kidscafe.domain.cafe.dto.response.NaverCafeResponseDto;
 import com.sparta.kidscafe.domain.cafe.service.CafeService;
+import com.sparta.kidscafe.domain.cafe.service.NaverMapService;
 import jakarta.validation.Valid;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class CafeController {
 
-  private final CafeService cafeService;
+    private final CafeService cafeService;
+    private final NaverMapService naverMapService;
 
-  @PostMapping("/owners/cafes")
-  public ResponseEntity<StatusDto> createCafe(
-      @Auth AuthUser authUser,
-      @Valid @RequestBody CafeCreateRequestDto requestDto
+    @PostMapping("/owners/cafes")
+    public ResponseEntity<StatusDto> createCafe(
+            @Auth AuthUser authUser,
+            @Valid @RequestBody CafeCreateRequestDto requestDto
+    ) {
+        AuthValidationCheck.validOwner(authUser);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(cafeService.createCafe(authUser, requestDto));
+    }
+
+    @PostMapping("/admin/cafes")
+    public ResponseEntity<StatusDto> createCafe(
+            @Auth AuthUser authUser,
+            @Valid @RequestBody CafesSimpleCreateRequestDto requestDto
+    ) {
+        AuthValidationCheck.validAdmin(authUser);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(cafeService.creatCafe(authUser, requestDto));
+    }
+
+    @GetMapping("/cafes")
+    public ResponseEntity<PageResponseDto<CafeResponseDto>> searchCafe(
+            @RequestBody CafeSearchRequestDto requestDto
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cafeService.searchCafe(requestDto.getSearchCondition()));
+    }
+
+    @GetMapping("/owners/cafes")
+    public ResponseEntity<PageResponseDto<CafeResponseDto>> searchCafeByOwner(
+            @Auth AuthUser authUser,
+            @RequestBody CafeSearchRequestDto requestDto
+    ) {
+        AuthValidationCheck.validOwner(authUser);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cafeService.searchCafe(requestDto.getSearchCondition()));
+    }
+
+    @GetMapping("/admin/cafes")
+    public ResponseEntity<PageResponseDto<CafeResponseDto>> searchCafeByAdmin(
+            @Auth AuthUser authUser,
+            @RequestBody CafeSearchRequestDto requestDto
+    ) {
+        AuthValidationCheck.validAdmin(authUser);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cafeService.searchCafe(requestDto.getSearchCondition()));
+    }
+
+    @GetMapping("/cafes/{cafeId}")
+    public ResponseEntity<ResponseDto<CafeDetailResponseDto>> findCafe(
+            @PathVariable Long cafeId
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cafeService.findCafe(cafeId));
+    }
+
+    @PatchMapping("/cafes/{cafeId}")
+    public ResponseEntity<StatusDto> updateCafe(
+            @Auth AuthUser authUser,
+            @PathVariable Long cafeId,
+            @Valid @RequestBody CafeSimpleRequestDto requestDto
+    ) {
+        AuthValidationCheck.validOwner(authUser);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cafeService.updateCafe(authUser, cafeId, requestDto));
+    }
+
+    @DeleteMapping("owners/cafes/{cafeId}")
+    public ResponseEntity<Void> deleteCafe(
+            @Auth AuthUser authUser,
+            @PathVariable Long cafeId
+    ) {
+        AuthValidationCheck.validOwner(authUser);
+        cafeService.deleteCafe(authUser, cafeId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("admin/cafes")
+    public ResponseEntity<Void> deleteCafe(
+            @Auth AuthUser authUser,
+            @RequestBody List<Long> cafes
+    ) {
+        AuthValidationCheck.validAdmin(authUser);
+        cafeService.deleteCafe(authUser, cafes);
+        return ResponseEntity.noContent().build();
+    }
+
+  @GetMapping("/naver/cafes")
+  public ResponseEntity<List<NaverCafeResponseDto>> searchCafesFromNaver(
+          @RequestParam String query,
+          @RequestParam String region
   ) {
-    AuthValidationCheck.validOwner(authUser);
+    List<NaverCafeResponseDto> cafes = naverMapService.searchCafes(query, region);
     return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(cafeService.createCafe(authUser, requestDto));
-  }
-
-  @PostMapping("/admin/cafes")
-  public ResponseEntity<StatusDto> createCafe(
-      @Auth AuthUser authUser,
-      @Valid @RequestBody CafesSimpleCreateRequestDto requestDto
-  ) {
-    AuthValidationCheck.validAdmin(authUser);
-    return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(cafeService.creatCafe(authUser, requestDto));
-  }
-
-  @GetMapping("/cafes")
-  public ResponseEntity<PageResponseDto<CafeResponseDto>> searchCafe(
-      @RequestBody CafeSearchRequestDto requestDto
-  ) {
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(cafeService.searchCafe(requestDto.getSearchCondition()));
-  }
-
-  @GetMapping("owners/cafes")
-  public ResponseEntity<PageResponseDto<CafeResponseDto>> searchCafeByOwner(
-      @Auth AuthUser authUser,
-      @RequestBody CafeSearchRequestDto requestDto
-  ) {
-    AuthValidationCheck.validOwner(authUser);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(cafeService.searchCafe(requestDto.getSearchCondition()));
-  }
-
-  @GetMapping("admin/cafes")
-  public ResponseEntity<PageResponseDto<CafeResponseDto>> searchCafeByAdmin(
-      @Auth AuthUser authUser,
-      @RequestBody CafeSearchRequestDto requestDto
-  ) {
-    AuthValidationCheck.validAdmin(authUser);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(cafeService.searchCafe(requestDto.getSearchCondition()));
-  }
-
-  @GetMapping("/cafes/{cafeId}")
-  public ResponseEntity<ResponseDto<CafeDetailResponseDto>> findCafe(
-      @PathVariable Long cafeId
-  ) {
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(cafeService.findCafe(cafeId));
-  }
-
-  @PatchMapping("/cafes/{cafeId}")
-  public ResponseEntity<StatusDto> updateCafe(
-      @Auth AuthUser authUser,
-      @PathVariable Long cafeId,
-      @Valid @RequestBody CafeSimpleRequestDto requestDto
-  ) {
-    AuthValidationCheck.validOwner(authUser);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(cafeService.updateCafe(authUser, cafeId, requestDto));
-  }
-
-  @DeleteMapping("owners/cafes/{cafeId}")
-  public ResponseEntity<Void> deleteCafe(
-      @Auth AuthUser authUser,
-      @PathVariable Long cafeId
-  ) {
-    AuthValidationCheck.validOwner(authUser);
-    cafeService.deleteCafe(authUser, cafeId);
-    return ResponseEntity.noContent().build();
-  }
-
-  @DeleteMapping("admin/cafes")
-  public ResponseEntity<Void> deleteCafe(
-      @Auth AuthUser authUser,
-      @RequestBody List<Long> cafes
-  ) {
-    AuthValidationCheck.validAdmin(authUser);
-    cafeService.deleteCafe(authUser, cafes);
-    return ResponseEntity.noContent().build();
+            .status(HttpStatus.OK)
+            .body(cafes);
   }
 }
