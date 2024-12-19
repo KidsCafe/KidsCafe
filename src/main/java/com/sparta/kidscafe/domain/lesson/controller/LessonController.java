02d533515fa143cca3104e501a0f18f16844f89b
guguggagga <gaeul79_@naver.com>
2024-12-19 20:31:38 +0900

    feat: 활동 클래스 조회 기능 구현 - 활동 클래스 조회 기능 구현 - 테스트 코드 추가

--- src/main/java/com/sparta/kidscafe/domain/lesson/controller/LessonController.java
+++ src/main/java/com/sparta/kidscafe/domain/lesson/controller/LessonController.java
@@ -2,14 +2,17 @@ package com.sparta.kidscafe.domain.lesson.controller;
 
 import com.sparta.kidscafe.common.annotation.Auth;
 import com.sparta.kidscafe.common.dto.AuthUser;
+import com.sparta.kidscafe.common.dto.ListResponseDto;
 import com.sparta.kidscafe.common.dto.StatusDto;
 import com.sparta.kidscafe.common.util.valid.AuthValidationCheck;
 import com.sparta.kidscafe.domain.lesson.dto.request.LessonCreateRequestDto;
+import com.sparta.kidscafe.domain.lesson.dto.response.LessonResponseDto;
 import com.sparta.kidscafe.domain.lesson.service.LessonService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +26,7 @@ public class LessonController {
   private final LessonService lessonService;
 
   @PostMapping("/cafes/{cafeId}/lessons")
-  public ResponseEntity<StatusDto> createLessons(
+  public ResponseEntity<StatusDto> createLesson(
       @Auth AuthUser authUser,
       @PathVariable Long cafeId,
       @Valid @RequestBody LessonCreateRequestDto requestDto
@@ -32,5 +35,16 @@ public class LessonController {
     return ResponseEntity
         .status(HttpStatus.CREATED)
         .body(lessonService.createLesson(authUser, cafeId, requestDto));
+  }
+
+  @GetMapping("/cafes/{cafeId}/lessons")
+  public ResponseEntity<ListResponseDto<LessonResponseDto>> searchLesson(
+      @Auth AuthUser authUser,
+      @PathVariable Long cafeId
+  ) {
+    AuthValidationCheck.validOwner(authUser);
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(lessonService.searchLesson(authUser, cafeId));
   }
 }
