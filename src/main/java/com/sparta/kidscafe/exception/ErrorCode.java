24192d21498f270b8ab68787cf28552608f00655
Do-hyeon Park <parkyj3213@naver.com>
2024-12-22 16:25:53 +0900

    feat: Coupon ErrorCode 등록

--- src/main/java/com/sparta/kidscafe/exception/ErrorCode.java
+++ src/main/java/com/sparta/kidscafe/exception/ErrorCode.java
@@ -43,7 +43,7 @@ public enum ErrorCode {
   PRICE_POLICY_MISMATCH("PRICE_POLICY_MISMATCH", "카페에 해당하지 않는 가격 정책입니다.", HttpStatus.BAD_REQUEST),
 
   // FEE 관련 에러
-  FEE_TABLE_UNAUTHORIZED("FEE_TABLE_UNAUTHORIZED", "일반유저는 가격책정의 권한이 없습니다.", HttpStatus.FORBIDDEN),
+  FEE_TABLE_UNAUTHORIZED("FEE_TABLE_UNAUTHORIZED", "일반 사용자는 가격책정의 권한이 없습니다.", HttpStatus.FORBIDDEN),
   FEE_TABLE_OWN_CREATE("FEE_TABLE_OWN_CREATE", "사장님 본인 가게에만 가격책정이 가능합니다.", HttpStatus.FORBIDDEN),
   FEE_NOT_FOUND("FEE_NOT_FOUND", "해당 요금을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 
@@ -55,6 +55,11 @@ public enum ErrorCode {
   REPORT_NOT_FOUND("REPORT_NOT_FOUND", "신고를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   INVALID_REPORT_STATUS("INVALID_REPORT_STATUS", "잘못된 신고 상태입니다.", HttpStatus.BAD_REQUEST),
   REPORT_STATUS_NOT_CHANGEABLE("REPORT_STATUS_NOT_CHANGEABLE", "현재 상태에서 요청된 상태로 변경할 수 없습니다.", HttpStatus.BAD_REQUEST),
+
+  // Coupon 관련 에러
+  COUPON_TABLE_UNAUTHORIZED("COUPON_TABLE_UNATHORIZED", "일반 사용자는 쿠폰 발급의 권한이 없습니다.", HttpStatus.FORBIDDEN),
+  COUPON_TABLE_OWN_CREATE("COUPON_TABLE_OWN_CREATE", "사장님 본인 가게에만 쿠폰 발급이 가능합니다.", HttpStatus.FORBIDDEN),
+  COUPON_NOT_FOUND("COUPON_NOT_FOUND", "해당 쿠폰을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 
   // 공통 에러
   FAIL_ENCODING("FAIL_ENCODING", "잘못된 인코딩을 사용하였습니다.", HttpStatus.BAD_REQUEST),
