5f649e2eeebee3f0773eb850e73162a2da333d23
KIMHYEJIN <0411khj@naver.com>
2024-12-23 02:56:15 +0900

    refactor: ReceiptScanController URL 매핑 수정 및 ReviewController 중복 메서드 제거

--- src/main/java/com/sparta/kidscafe/domain/review/controller/ReceiptScanController.java
+++ src/main/java/com/sparta/kidscafe/domain/review/controller/ReceiptScanController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/reviews")
+@RequestMapping("/api/receipt")
 public class ReceiptScanController {
 
   private final ReceiptScanService receiptScanService;
