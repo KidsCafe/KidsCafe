5319159a3ad848161b3758c2b937315c6edf4c0a
KIMHYEJIN <0411khj@naver.com>
2024-12-26 20:54:37 +0900

    test: CafeImageServiceTest 수정 - Cafe 객체 기반 이미지 삭제 검증 로직 반영 #147

--- src/test/java/com/sparta/kidscafe/domain/cafe/service/CafeImageServiceTest.java
+++ src/test/java/com/sparta/kidscafe/domain/cafe/service/CafeImageServiceTest.java
@@ -48,7 +48,7 @@ public class CafeImageServiceTest {
   @Mock
   private FileStorageUtil fileUtil;
 
-  private final String dirPath = "http://sparta.com/mock/images";
+  private final String dirPath = "https://sparta.com/mock/images";
 
   @BeforeEach
   void setUp() {
@@ -113,7 +113,7 @@ public class CafeImageServiceTest {
     // then
     verify(cafeValidationCheck).validMyCafe(cafeId, authUser.getId());
     verify(cafeImageRepository).findAllById(imageIds);
-    assertNull(cafeImages.get(0).getCafeId());
-    assertNull(cafeImages.get(1).getCafeId());
+    assertNull(cafeImages.get(0).getCafe());
+    assertNull(cafeImages.get(1).getCafe());
   }
 }
