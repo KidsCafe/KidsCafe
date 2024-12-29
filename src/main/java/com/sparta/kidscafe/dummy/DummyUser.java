7c193b45bfda74643f8e2a3080cc44b845b6502b
guguggagga <gaeul79_@naver.com>
2024-12-29 15:29:29 +0900

    test: 리뷰 이미지 테스트 코드 수정

--- src/main/java/com/sparta/kidscafe/dummy/DummyUser.java
+++ src/main/java/com/sparta/kidscafe/dummy/DummyUser.java
@@ -1,5 +1,6 @@
 package com.sparta.kidscafe.dummy;
 
+import com.sparta.kidscafe.common.dto.AuthUser;
 import com.sparta.kidscafe.common.enums.LoginType;
 import com.sparta.kidscafe.common.enums.RoleType;
 import com.sparta.kidscafe.common.util.PasswordEncoder;
@@ -10,6 +11,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DummyUser {
+
+  public static User createDummyUser(Long id, RoleType roleType) {
+    PasswordEncoder passwordEncoder = new PasswordEncoder();
+    String randomName = TestUtil.getRandomString(10);
+    String randomNickname = TestUtil.getRandomString(10);
+    String randomEmail = randomNickname + "@email.com";
+    String randomPassword = passwordEncoder.encode("1234");
+    return User.builder()
+        .id(id)
+        .email(randomEmail)
+        .password(randomPassword)
+        .nickname(randomNickname)
+        .name(randomName)
+        .address("대한민국 어딘가")
+        .role(roleType)
+        .loginType(LoginType.BASIC)
+        .build();
+  }
 
   public static User createDummyUser(RoleType roleType) {
     PasswordEncoder passwordEncoder = new PasswordEncoder();
--- src/test/java/com/sparta/kidscafe/domain/review/service/ReviewImageServiceTest.java
+++ src/test/java/com/sparta/kidscafe/domain/review/service/ReviewImageServiceTest.java
@@ -21,32 +21,45 @@ import com.sparta.kidscafe.domain.review.repository.ReviewImageRepository;
 import com.sparta.kidscafe.domain.review.repository.ReviewRepository;
 import com.sparta.kidscafe.domain.user.entity.User;
 import com.sparta.kidscafe.dummy.DummyCafe;
+import com.sparta.kidscafe.dummy.DummyReview;
 import com.sparta.kidscafe.dummy.DummyReviewImage;
 import com.sparta.kidscafe.dummy.DummyUser;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.MultipartFile;
 
-@ExtendWith(MockitoExtension.class) // Mockito 초기화 자동 설정
 public class ReviewImageServiceTest {
 
   @InjectMocks
   private ReviewImageService reviewImageService;
+
   @Mock
   private FileStorageUtil fileUtil;
+
   @Mock
   private ReviewRepository reviewRepository;
+
   @Mock
   private ReviewImageRepository reviewImageRepository;
+
+  private String dirPath = "https://sparta.com/mock/images/";
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
 
   private AuthUser createAuthUser() {
     return new AuthUser(1L, "hong@email.com", RoleType.USER);
@@ -56,32 +69,26 @@ public class ReviewImageServiceTest {
   @DisplayName("이미지 업로드 성공")
   void uploadReviewImage_Success() throws IOException {
     // given - user
-    String dirPath = "https://sparta.com/mock/images"; // 지역 변수로 변경
+    Long cafeId = 1L;
     Long reviewId = 1L;
     AuthUser authUser = createAuthUser();
     User user = DummyUser.createDummyUser(authUser.getRoleType());
-    Cafe cafe = DummyCafe.createDummyCafe(user, reviewId);
-
-    // 빌더를 이용한 Review 객체 생성
-    Review review = Review.builder()
-        .id(reviewId)
-        .user(user)
-        .cafe(cafe)
-        .star(5.0)
-        .content("Great!")
-        .depth(0)
-        .build();
+    Cafe cafe = DummyCafe.createDummyCafe(user, cafeId);
+    Review review = DummyReview.createDummyReview(reviewId, user, cafe);
 
     // given - image
+    dirPath += authUser.getId() + "/" + ImageType.REVIEW.toString().toLowerCase() + "/";
     String imagePath = dirPath + "image.jpg";
     MultipartFile mockImage = mock(MultipartFile.class);
     List<MultipartFile> images = List.of(mockImage);
+    List<ReviewImage> reviewImages = new ArrayList<>();
     ReviewImage reviewImage = DummyReviewImage.createDummyReviewImage(review, imagePath);
+    reviewImages.add(reviewImage);
 
     when(fileUtil.makeDirectory(ImageType.REVIEW, authUser.getId())).thenReturn(dirPath);
     when(fileUtil.makeFileName(dirPath, mockImage)).thenReturn(imagePath);
     doNothing().when(mockImage).transferTo(any(File.class));
-    when(reviewImageRepository.save(any(ReviewImage.class))).thenReturn(reviewImage);
+    when(reviewImageRepository.saveAll(reviewImages)).thenReturn(reviewImages);
 
     // when
     StatusDto result = reviewImageService.uploadImage(authUser.getId(), reviewId, images);
@@ -91,32 +98,23 @@ public class ReviewImageServiceTest {
     verify(fileUtil, times(1)).makeDirectory(ImageType.REVIEW, authUser.getId());
     verify(fileUtil, times(1)).makeFileName(dirPath, mockImage);
     verify(fileUtil, times(1)).uploadImage(imagePath, mockImage);
-    verify(reviewImageRepository, times(1)).save(any());
+    when(reviewImageRepository.saveAll(reviewImages)).thenReturn(reviewImages);
   }
 
   @Test
   @DisplayName("이미지 삭제 성공 (soft-delete)")
   void deleteImage_Success() {
     // given - user
+    Long cafeId = 1L;
     Long reviewId = 1L;
     AuthUser authUser = createAuthUser();
-    User user = DummyUser.createDummyUser(authUser.getRoleType());
-    Cafe cafe = DummyCafe.createDummyCafe(user, reviewId);
-
-    // 빌더를 이용한 Review 객체 생성
-    Review review = Review.builder()
-        .id(reviewId)
-        .user(user)
-        .cafe(cafe)
-        .star(5.0)
-        .content("Great!")
-        .depth(0)
-        .build();
+    User user = DummyUser.createDummyUser(authUser.getId(), authUser.getRoleType());
+    Cafe cafe = DummyCafe.createDummyCafe(user, cafeId);
+    Review review = DummyReview.createDummyReview(reviewId, user, cafe);
 
     // given - image
     Long imageId = 1L;
     ReviewImage reviewImage = DummyReviewImage.createDummyReviewImage(imageId, review);
-
     when(reviewImageRepository.findById(imageId)).thenReturn(Optional.of(reviewImage));
     when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
 
@@ -125,6 +123,5 @@ public class ReviewImageServiceTest {
 
     // then
     verify(reviewImageRepository).findById(imageId);
-    assertNull(reviewImage.getReviewId());
   }
 }
