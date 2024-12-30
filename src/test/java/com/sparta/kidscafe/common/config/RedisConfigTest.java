c16c7d6e91025d3b550b256fe3b5a7bfdffd4f72
Jisoo.R <jisuryu0527@gmail.com>
2024-12-30 15:58:14 +0900

    test(search): Redis 연결 확인용 테스트 코드 #151

--- src/test/java/com/sparta/kidscafe/common/config/RedisConfigTest.java
+++ src/test/java/com/sparta/kidscafe/common/config/RedisConfigTest.java
@@ -3,7 +3,6 @@ package com.sparta.kidscafe.common.config;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
