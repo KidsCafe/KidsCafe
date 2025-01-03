8cafe17d5b64cddc9e71aa7e84a9d6b4274d5e1e
Jisoo.R <jisuryu0527@gmail.com>
2025-01-03 23:08:55 +0900

    feat(search-entity): 카페 검색 쿼리 수정 #151
    * '=' -> 'Like'로 변경: 특정 키워드를 포함하는 데이터 검색
    * JPA 문법에 따른 메서드 이름 수정
    * Native Query -> Pageable 사용

--- src/main/java/com/sparta/kidscafe/domain/cafe/repository/CafeViewCountRepository.java
+++ src/main/java/com/sparta/kidscafe/domain/cafe/repository/CafeViewCountRepository.java
@@ -4,6 +4,8 @@ import com.sparta.kidscafe.domain.cafe.entity.Cafe;
 import com.sparta.kidscafe.domain.cafe.entity.CafeViewCount;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,13 +16,13 @@ public interface CafeViewCountRepository extends JpaRepository<CafeViewCount, Lo
 
   Optional<CafeViewCount> findByCafe(Cafe cafe);
 
-  // Redis용
-  @Query("SELECT c FROM CafeViewCount c WHERE c.cafe.region = :region ORDER BY c.viewCount DESC")
+  // 지역별 모든 CafeViewCount 조회
+  @Query("SELECT c FROM CafeViewCount c WHERE c.cafe.region LIKE :region% ORDER BY c.viewCount DESC")
   List<CafeViewCount> findAllByRegion(@Param("region") String region);
 
-  // DB용
-  @Query("SELECT c FROM CafeViewCount c WHERE c.cafe.region = :region ORDER BY c.viewCount DESC limit 5")
-  List<CafeViewCount> findTop5Cafe_RegionOrderByViewCountDesc(String region);
+  // 지역별 상위 N개(페이지) 조회
+  @Query(value = "SELECT c FROM CafeViewCount c WHERE c.cafe.region LIKE :region% ORDER BY c.viewCount DESC")
+  Page<CafeViewCount> findCafesByRegionOrderByViewCountDesc(@Param("region") String region, Pageable pageable);
 
   List<CafeViewCount> findAllByCafeIdInOrderByViewCountDesc(List<Long> cafeIds);
 }
