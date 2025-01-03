c4275acd0f7f419b3c5237c52b99f8669f01bd29
Jisoo.R <jisuryu0527@gmail.com>
2025-01-03 23:05:26 +0900

    refactor(lesson): import문 정리, Repository 어노테이션 추가

--- src/main/java/com/sparta/kidscafe/domain/lesson/repository/LessonRepository.java
+++ src/main/java/com/sparta/kidscafe/domain/lesson/repository/LessonRepository.java
@@ -2,10 +2,12 @@ package com.sparta.kidscafe.domain.lesson.repository;
 
 
 import com.sparta.kidscafe.domain.lesson.entity.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
   List<Lesson> findAllByCafeId(Long cafeId);
 }
